f57471eb5741e2dd49253e917da0c290545cc00a
John Shewchuk <johnshew@users.noreply.github.com>
2018-11-24 16:04:47 -0800

    Login and retrieval working

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { ActivityTypes, RecognizerResult, TurnContext, CardFactory } from 'botbuilder';
+import { ActivityTypes, RecognizerResult, TurnContext, CardFactory, BrowserLocalStorage } from 'botbuilder';
 import { LuisApplication, LuisPredictionOptions, LuisRecognizer } from 'botbuilder-ai';
+import { Client as GraphClient } from '@microsoft/microsoft-graph-client';
 
 /**
  * A simple bot that responds to utterances with answers from the Language Understanding (LUIS) service.
@@ -16,28 +17,49 @@ export class SimpleBot {
      * response, with no stateful conversation.
      * @param turnContext A TurnContext instance, containing all the data needed for processing the conversation turn.
      */
-    public async onTurn(turnContext: TurnContext) {
+    async onTurn(turnContext: TurnContext) {
         // By checking the incoming Activity type, the bot only calls LUIS in appropriate cases.
-        if (turnContext.activity.type === ActivityTypes.Message) {
-            switch (turnContext.activity.text.toLowerCase().trim()) {
-                case 'login':
-                await turnContext.sendActivity('Sending an oauthCard');
-                await turnContext.sendActivity({ attachments: [CardFactory.oauthCard("AAD-OAUTH", 'title', 'text')]});
-                    return;
-                default:
-                    await turnContext.sendActivity(`Hello`);
-                    return;
-            }
+        const activity = turnContext.activity;
+        switch (turnContext.activity.type) {
+            case ActivityTypes.Message:
+                switch (activity.text.toLowerCase().trim()) {
+                    case 'login':
+                        await turnContext.sendActivity('Sending an oauthCard');
+                        await turnContext.sendActivity({ attachments: [CardFactory.oauthCard("AAD-OAUTH", 'title', 'text')] });
+                        return;
+                    default:
+                        await turnContext.sendActivity(`Hello`);
+                        return;
+                }
+                break;
 
-        } else if (turnContext.activity.type === ActivityTypes.ConversationUpdate &&
-            turnContext.activity.recipient.id !== turnContext.activity.membersAdded[0].id) {
-            // If the Activity is a ConversationUpdate, send a greeting message to the user.
-            await turnContext.sendActivity('Welcome to the NLP with LUIS sample! Send me a message and I will try to predict your intent.');
-        } else if (turnContext.activity.type !== ActivityTypes.ConversationUpdate) {
-            // Respond to all other Activity types.
-            await turnContext.sendActivity(`[${turnContext.activity.type}]-type activity detected.`);
-        } else {
-            await turnContext.sendActivity(`Activity type: ${ turnContext.activity.type } `);
+            case ActivityTypes.ConversationUpdate:
+                if (activity.recipient.id !== turnContext.activity.membersAdded[0].id) {
+                    await turnContext.sendActivity('Welcome to the NLP with LUIS sample! Send me a message and I will try to predict your intent.');
+                }
+                break;
+
+            case ActivityTypes.Event:
+                if (activity.name && activity.name === "tokens/response" && activity.value.token) {
+                    await turnContext.sendActivity('Got a token');
+                    let token = activity.value.token;
+                    let graphClient = GraphClient.init({
+                        authProvider: (done) => {
+                            done(null, token); // First parameter takes an error if you can't get an access token.
+                        }
+                    });
+                    let result = await graphClient
+                        .api('/me')
+                        .get().then((res) => {
+                            return res;
+                        });
+                    await turnContext.sendActivity(`Result: ${JSON.stringify(result)}`);
+                }
+                break;
+
+            default:
+                await turnContext.sendActivity(`[${turnContext.activity.type}]-type activity detected.`);
+                break;
         }
     }
 }
