7cb28a8a8d75ac3129b3090370c4a1ca7fcefb9c
John Shewchuk <johnshew@users.noreply.github.com>
2018-12-06 12:48:14 -0800

    Index to app

--- src/app.ts
+++ src/app.ts
@@ -45,3 +45,19 @@ const botService = new SimpleBotService(NagBot, app.appId, app.appPassword, botP
 app.bot = botService.bot;
 
 app.httpServer = new httpServer.Server(httpServerPort);
+
+function tick() {
+    console.log('In tick');
+    let users = app.users;
+    users.forEach(async u => {
+        try {
+            let accessToken = await app.authManager.accessTokenForAuthKey(u.authKey);
+            let result = await app.graphHelper.get(accessToken, "https://graph.microsoft.com/v1.0/me/");
+            console.log(`User: ${JSON.stringify(result)} `);
+        } catch (err) {
+            console.log(`Error in tick: ${err}`);
+        }
+    });
+}
+
+setInterval(() => tick(), 9 * 1000);
