3c2ae67cb54a7f7b6de97dca80d2db8fe1b534fa
John Shewchuk <johnshew@users.noreply.github.com>
2019-01-16 20:34:12 -0800

    Write NagLast

--- src/notifications.ts
+++ src/notifications.ts
@@ -1,33 +1,40 @@
 import { app } from './app';
+import { SingleValueLegacyExtendedProperty } from '@microsoft/microsoft-graph-types-beta';
 
 function checkPolicy(policy: any, dueDate: Date, lastNagDate: Date): { notify: boolean, daysUntilDue: number } {
     let daysSinceNag = Math.trunc((dueDate.valueOf() - lastNagDate.valueOf()) / (1000 * 60 * 60 * 24));  // Should convert to UTC to do this calc.
     let daysUntilDue = Math.trunc((dueDate.valueOf() - Date.now()) / (1000 * 60 * 60 * 24));
-    return { notify: (daysSinceNag > 0) , daysUntilDue: daysUntilDue };
+    if (policy == 'base') return { notify: (daysSinceNag > 0) , daysUntilDue: daysUntilDue };
+    else return { notify: true, daysUntilDue: daysUntilDue };
 }
 
 export async function notify() {
-    let users = app.users;
-    app.users && users.forEach(async (user, key) => {
+    if (!app.users) return;
+    for await (const [oid, user] of app.users) {
         try {
-            let oid = user.oid;
             let accessToken = await app.authManager.accessTokenForOid(oid);
             console.log(`User: ${oid}`);
             let tasks = await app.graph.getNagTasks(accessToken);
-            for (const task of tasks) {
+            for await (const task of tasks) {
                 let dueDate = new Date(Date.parse(task.dueDateTime && task.dueDateTime.dateTime));
                 let lastNag = task.singleValueExtendedProperties && task.singleValueExtendedProperties.find((i) => i.id.split(' ')[3] == "NagLast");
-                let lastNagDate = lastNag ? (new Date(Date.parse(lastNag.value))) : new Date(0);
-                let policy = checkPolicy('base', dueDate, lastNagDate);
+                let lastNagDate = lastNag && lastNag.value ? (new Date(Date.parse(lastNag.value))) : new Date(0);
+                let policy = checkPolicy('always', dueDate, lastNagDate);
                 if (!policy.notify) return;
-
                 let conversations = app.conversationManager.findAllConversations(oid);
-                for (const c of conversations) {
-                    await app.conversationManager.processActivityInConversation(app.adapter, c, async turnContext => {
+                for await (const conversation of conversations) {
+                    await app.conversationManager.processActivityInConversation(app.adapter, conversation, async turnContext => {
                         let dueMessage = (policy.daysUntilDue > 0) ? `due in ${policy.daysUntilDue} days` : `overdue by ${-policy.daysUntilDue} days`;
-                        await turnContext.sendActivity(
-                            `Task: ${task.subject} ${dueMessage}`);
-                        // update NagLast with current time.
+                        let now = new Date(Date.now()).toISOString();
+                        if (lastNag) { lastNag.value = now; }
+                        else { 
+                            if (!task.singleValueExtendedProperties) { task.singleValueExtendedProperties = []; }
+                            let property : SingleValueLegacyExtendedProperty = { id: 'String {d0ac6527-76d0-4eac-af0b-b0155e8ad503} Name NagLast', value: now };
+                            task.singleValueExtendedProperties.push(property);
+                        }
+                        await turnContext.sendActivity(`Task: ${task.subject} ${dueMessage}`);
+                        let body = { singleValueExtendedProperties: task.singleValueExtendedProperties };
+                        await app.graph.patch(accessToken,`https://graph.microsoft.com/beta/me/outlook/tasks/${task.id}`, body);                        
                     });
                 }
             }
@@ -36,5 +43,5 @@ export async function notify() {
             console.log(`Error in notify: ${err}`);
         }
         return;
-    });
+    };
 }
