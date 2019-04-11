a12877d8c2ca4aa5d9b22ba67f90ec1240d8cdeb
John Shewchuk <johnshew@users.noreply.github.com>
2019-04-11 10:52:13 -0700

    Updated tests

--- src/__tests__/conversations.test.ts
+++ src/__tests__/conversations.test.ts
@@ -1,9 +1,29 @@
-
-import { ConversationManager } from '../conversations';
+import { ConversationReference } from 'botbuilder'
+import { ConversationManager, compare } from '../conversations';
 
 describe("Users", () => {
-    test("simple", async(done) => {
+    test("simple", async (done) => {
         let conversations = new ConversationManager();
+        let oid = '1234'
+        expect(conversations.findAll(oid)).toHaveLength(0);
+
+        let item: Partial<ConversationReference> = {
+            serviceUrl: 'about:', channelId: "test",
+            conversation: { isGroup: false, conversationType: 'foo', id: 'foo', name: 'foo', tenantId: 'foo' }
+        }
+        conversations.load(oid, [item]);
+        expect(conversations.findAll(oid)).toHaveLength(1);
+        expect(conversations.find(oid, (x) => compare(x, item))).toBeDefined();
+
+        let called = false;
+        conversations.on('updated', (oid, conversation) => { called = true });
+
+        let altItem: Partial<ConversationReference> = { ...item, conversation: { ...item.conversation } };
+        let altOid = '5678';
+        conversations.addUnauthenticatedConversation('xyzzy', altItem);
+        conversations.setOidForUnauthenticatedConversation('xyzzy', altOid);
+        expect(called).toBe(true);
+        expect(conversations.findAll(altOid)).toHaveLength(1);
         done();
     })
 });
