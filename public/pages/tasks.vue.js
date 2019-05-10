9bd6cd5c87434638fc73598ce2769d6c7895d2de
John Shewchuk <johnshew@users.noreply.github.com>
2019-05-09 22:41:08 -0700

    Alignment

--- public/pages/tasks.vue.js
+++ public/pages/tasks.vue.js
@@ -2,16 +2,16 @@ var spaTasks = Vue.component("Tasks", {
     template: /*html*/
 `<v-container fluid>
     <template v-for="(task, index) in tasks">
-        <v-layout row>
+        <v-layout row align-center>
             <v-flex shrink pa-1>
-                <v-icon v-if="task.status === 'notStarted'">radio_button_checked</v-icon>
-                <v-icon v-else>radio_button_unchecked</v-icon>
+                <v-icon v-if="task.status === 'notStarted'" color="blue">check_circle_outline</v-icon>
+                <v-icon v-else color="blue">radio_button_unchecked</v-icon>
             </v-flex>
             <v-flex grow pa-1 >
                 <router-link :to="{ path: '/task/'+task.id }" style="text-decoration:none" headline> {{ task.subject }}</router-link>
-                {{ new Date(task.dueDateTime.dateTime).toLocaleString() }}
+                <div>{{ new Date(task.dueDateTime.dateTime).toLocaleString() }}</div>
             </v-flex>
-            <v-flex shrink pa-1>
+            <v-flex shrink pa-1 align-center>
                 <v-icon v-if="!task.categories.includes('NagMe')" color="grey lighten-1"> alarm_off </v-icon>
                 <v-icon v-else color="green darken-1"> alarm_on </v-icon>
             </v-flex>
