080c31becb9abf354532cc1c5666bd8ad9ce608d
John Shewchuk <johnshew@users.noreply.github.com>
2019-05-12 10:10:18 -0700

    More complete display and edit

--- public/pages/task.vue.js
+++ public/pages/task.vue.js
@@ -1,17 +1,33 @@
 var spaTask = Vue.component("Task", {
-    template: /*html*/`<div>
-    <div style="margin-bottom: 10px;"></div>
-    <div v-if="task">
-        <br />
-        <h3>{{ task.subject }}</h3>
-        <p>{{ new Date(task.dueDateTime.dateTime).toDateString() }}</p>
-        <p>
+    template:
+/*html*/`<v-container fluid>
+    <v-layout row align-top pa-1 v-if="task">
+        <v-flex shrink align-top pa-1>
+            <v-icon v-if="!task.categories.includes('NagMe')" color="grey lighten-1" @click="NagChange(task)"> alarm_off
+            </v-icon>
+            <v-icon v-else color="green darken-1" @click="NagChange(task)"> alarm_on </v-icon>
+        </v-flex>
+        <v-flex shrink align-top pa-1>
+            <v-icon v-if="task.status === 'notStarted'" color="blue" @click="StatusChange(task)">check_circle_outline
+            </v-icon>
+            <v-icon v-else color="blue" @click="StatusChange(task)">radio_button_unchecked</v-icon>
+        </v-flex>
+        <v-flex grow pa-1>
+            <div>{{ task.subject }}</div>
+            <p>{{ new Date(task.dueDateTime.dateTime).toDateString() }}  {{ task.status }}</p>
+            <div v-html="task.body.content"></div>
             <template v-for="category in task.categories">
-                {{category}}
-            </template>
-        </p>
-    </div>
-</div>`,
+            <v-chip>
+            {{category}}
+            </v-chip>
+                </template>
+        
+        </v-flex>
+    </v-layout>
+    <v-layout row pa-1 v-if="ready && !task">
+    Nothing to show.<router-link to="/login">Login</router-link>
+    </v-layout>
+</v-container fluid>`,
     props: ["id"],
     data() {
         return {
@@ -39,6 +55,7 @@ var spaTask = Vue.component("Task", {
                 })
                 .catch(err => {
                     console.error('Error', err);
+                    this.ready = true
                 });
 
         },
@@ -50,6 +67,30 @@ var spaTask = Vue.component("Task", {
             idStart += prefix.length;
             id = path.substring(idStart).trim();
             return id;
+        },
+        UpdateTask(task) {
+            let patch = { status: task.status, categories: task.categories };
+            window.fetch(`/api/v1.0/tasks/${task.id}`, {
+                method: "PATCH",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(patch),
+            })
+                .then(response => {
+                    return response.json();
+                })
+                .then(json => {
+                    task = json;
+                })
+                .catch(err => console.error(err))
+        },
+        StatusChange(task) {
+            task.status = (task.status === "completed") ? "notStarted" : "completed";
+            this.UpdateTask(task);
+        },
+        NagChange: async function (task) {
+            if (task.categories.includes('NagMe')) { task.categories = task.categories.filter(category => category !== 'NagMe') }
+            else { task.categories.splice(0, 0, 'NagMe'); }
+            this.UpdateTask(task);
         }
     }
 });
