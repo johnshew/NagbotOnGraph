0e81a19fe36db78ee7ea940b7efa04c247c3a801
James Trott <jatrott@microsoft.com>
2019-05-24 11:43:55 -0700

    updated helm bootstrap to properly deploy a cluster with helm, and added bootstrapping for prometheus and grafana

--- deploy/bootstrap-helm.sh
+++ deploy/bootstrap-helm.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Still in dev, may be unreliable.
 set -x
+version=1.0
+echo "Nagbot Telemetry Bootstrap: $version"
 
 #config values
-export RgName=jtscript17
-export ClusterName=jtcluster17
+export RgName=jttelemdev
+export ClusterName=telemcluster1
 export NodeCount=1
+export MonitoringNamespace
 
 echo cluster-name
 
@@ -16,25 +19,41 @@ az aks install-cli #(TODO: Implement check to see if we need Kubectl)
 az aks get-credentials --resource-group=$RgName --name=$ClusterName
 
 #Install Helm (TODO: Implement check to see if we already have helm.)
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash # installs latest because we like to live on the edge.
+curl -L https://git.io/get_helm.sh | bash # installs latest because we like to live on the edge.
 
-#Create the service account for tiller on the cluster
+#Create the service account for tiller on the cluster to allow rbac access.
+#(This step MUST be done before helm is init on the cluster with current stable helm)
 kubectl apply -f helm-rbac.yaml
 
 #Init helm
 helm init --history-max=200 --service-account=tiller
 
-#Add Weaveworks chart repo.
-helm repo add weaveworks https://weaveworks.github.io/flux
+#Clone the helm charts repository
+git clone https://github.com/helm/charts.git
 
-#Deploy flux via Helm
-helm install --name flux \
---set git.url=git@github.com:jatrott/flux-get-started \
---namespace flux \
-weaveworks/flux
+#Switch to the chart folder for prometheus stable.
+cd charts/stable/prometheus
 
-#Install fluxCTL
-curl -L https://github.com/weaveworks/flux/releases/download/1.12.0/fluxctl_linux_amd64 -o /usr/local/bin/fluxctl
-chmod a+x /usr/local/bin/fluxctl
+#Install Prometheus
+helm install --name=prometheus . --namespace $MonitoringNamespace --set rbac.create=true
 
-#Inject correct key to flux
+#export name of the current prometheus pod to env var 'PROMETHEUS_POD_NAME'
+#to proxy to local machine for access use: 
+#kubectl --namespace monitoring port-forward $PROMETHEUS_POD_NAME 9090
+export PROMETHEUS_POD_NAME=$(kubectl get pods --namespace $MonitoringNamespace -l "app=prometheus,component=server" -o jsonpath="{.items[0].metadata.name}")
+
+#Switch to the chart folder for grafana stable.
+cd ../grafana
+
+#Install Grafana
+helm install --name=grafana . --set persistence.enabled=true --set persistence.accessModes={ReadWriteOnce} --set persistence.size=8Gi --namespace monitoring
+
+#Export name of current grafana pod to env var 'GRAFANA_POD_NAME'
+#to proxy to local machine for access use: 
+#kubectl --namespace monitoring port-forward $GRAFANA_POD_NAME 3000
+export GRAFANA_POD_NAME=$(kubectl get pods --namespace monitoring -l "app=grafana,release=grafana" -o jsonpath="{.items[0].metadata.name}")
+
+#return to execution root
+cd ../../..
+#cleanup charts directory cloned by script.
+rm -rf charts
